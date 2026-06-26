e2b62ba04b2eb9c352ddbae3fd2c501ae4e15c88
Kipra2003 <pratyush.kumar@kiwiinsurance.com>
2026-06-26 11:29:32 +0530

    database connected

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Inject } from '@nestjs/common';
+import { PG_CONNECTION } from './contants';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(PG_CONNECTION) private readonly db: any) {}
+
+  async getHello(): Promise<any> {
+    console.log('db', this.db.query);
+    const res = await this.db.query("SELECT CURRENT_TIME;");
+    console.log('res', res.rows);
+    return res.rows;
   }
 }
---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import {PG_CONNECTION} from '../contants';
+// import { ConfigService } from '@nestjs/config';
+
+const dbProvider = {
+  provide: PG_CONNECTION,
+  useFactory: async () => {
+    const { Pool } = await import('pg');
+    const pool = new Pool({
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: parseInt(process.env.PG_PORT || '5430', 10),
+    });
+    return pool;
+  },
+};
+
+
+
+@Module({
+  providers: [dbProvider],
+  exports: [dbProvider],
+})
+export class DatabaseModule {
+}
